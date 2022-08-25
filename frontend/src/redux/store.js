fbffadf873989f9dda4cad855818eda16d695bd2
giabao1511 <giabao712411@gmail.com>
2022-08-25 19:30:55 +0700

    update redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,22 +4,22 @@ import cartSlice from "./cartSlice";
 import productSlice from "./productSlice";
 
 let store;
-// if (process.env.NODE_ENV === "development") {
-//   store = configureStore({
-//     reducer: {
-//       cart: cartSlice,
-//       product: productSlice,
-//     },
+if (process.env.NODE_ENV === "development") {
+  store = configureStore({
+    reducer: {
+      cart: cartSlice,
+      product: productSlice,
+    },
 
-//     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-//   });
-// }
-
-store = configureStore({
-  reducer: {
-    cart: cartSlice,
-    product: productSlice,
-  },
-});
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  });
+} else {
+  store = configureStore({
+    reducer: {
+      cart: cartSlice,
+      product: productSlice,
+    },
+  });
+}
 
 export default store;
