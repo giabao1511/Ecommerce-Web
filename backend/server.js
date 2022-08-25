5ebd2a9afde05857dace07462fc49c1c66f48f44
giabao1511 <giabao712411@gmail.com>
2022-08-25 18:56:54 +0700

    push code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config();
 const cors = require("cors");
 const connectDB = require("./configs/connectDB");
 
